7a900014dd22d0072593388eb06de74ba87b0aab
Stone Preston <stone.preston@canonical.com>
2023-03-17 14:47:23 -0500

    Remove duplicate finalizer-related code

--- controllers/charmedk8scontrolplane_controller.go
+++ controllers/charmedk8scontrolplane_controller.go
@@ -218,25 +218,6 @@ func (r *CharmedK8sControlPlaneReconciler) Reconcile(ctx context.Context, req ct
 		return ctrl.Result{Requeue: true}, nil
 	}
 
-	// examine DeletionTimestamp to determine if object is under deletion
-	if kcp.ObjectMeta.DeletionTimestamp.IsZero() {
-		// The object is not being deleted, so if it does not have our finalizer,
-		// then lets add the finalizer and update the object. This is equivalent
-		// registering our finalizer.
-		if !controllerutil.ContainsFinalizer(kcp, controlplanev1beta1.CharmedK8sControlPlaneFinalizer) {
-			controllerutil.AddFinalizer(kcp, controlplanev1beta1.CharmedK8sControlPlaneFinalizer)
-			if err := r.Update(ctx, kcp); err != nil {
-				return ctrl.Result{}, err
-			}
-			log.Info("added finalizer")
-			return ctrl.Result{}, nil
-		}
-	} else {
-		// The control plane object is being deleted
-		log.Info("deleting control plane")
-		return r.reconcileDelete(ctx, cluster, kcp)
-	}
-
 	defer func() {
 		log.Info("attempting to set control plane status")
 
